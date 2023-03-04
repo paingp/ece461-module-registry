8126a69abbabfeb6c38a91fa42fb59a3dea7a81f
Matthew <campb474@purdue.edu>
2023-03-04 14:48:11 -0500

    Adding npm domain functionality

--- src/go/main.go
+++ src/go/main.go
@@ -8,6 +8,12 @@ import (
 	// "utils"
 	"fmt"
 	"os"
+	"encoding/json"
+
+	//"fmt"
+	"io"
+	"net/http"
+	"strings"
 )
 
 var (
@@ -24,6 +30,7 @@ func (NoLog) Write([]byte) (int, error) {
 func main() {
 
 	fmt.Println("Aditya Srikanth")
+	fmt.Println("Here now")
 
 	doLogging := true
 	logFileName := os.Getenv("LOG_FILE")
@@ -61,7 +68,11 @@ func main() {
 	// for each url in the file
 	for scanner.Scan() {
 		//Create new repositories with current URL scanned
-		hold = newRepo(scanner.Text())
+		var tempURL = scanner.Text()
+		tempURL = getGithubUrl(tempURL)
+		hold = newRepo(tempURL)
+		fmt.Println(tempURL)
+		fmt.Println("Here now\n\n\n")
 		InfoLogger.Println("New repo created successfully")
 		// Adds repository to Linked List in sorted order by net score
 		head = addRepo(head, head.next, hold)
@@ -69,4 +80,45 @@ func main() {
 
 	// Prints each repository in NDJSON format to stdout (sorted highest to low based off net score)
 	printRepo(head.next)
+}
+
+
+func getGithubUrl(url string) string {
+	before, after, found := strings.Cut(url, "www")
+	//Finding endpoints and checking for their existence
+	if found {
+		npmEndpoint := before + "registry" + after
+		npmEndpoint = strings.Replace(npmEndpoint, "com", "org", 1)
+		npmEndpoint = strings.Replace(npmEndpoint, "package/", "", 1)
+
+		resp, err := http.Get(npmEndpoint)
+
+		if err != nil {
+			return ""
+		}
+
+		if resp.StatusCode == http.StatusOK {
+			bodyBytes, err := io.ReadAll(resp.Body)
+
+			if err != nil {
+				return ""
+			}
+
+			bodyString := string(bodyBytes)
+			resBytes := []byte(bodyString)
+			var npmRes map[string]interface{}
+			_ = json.Unmarshal(resBytes, &npmRes)
+			
+
+			bugs := npmRes["bugs"].(map[string]interface{})
+			npmEndpoint = bugs["url"].(string)
+
+			if (npmEndpoint == ""){
+				return ""
+			}
+
+			url = strings.Replace(npmEndpoint, "/issues", "", 1)
+		}
+	}
+	return url
 }
