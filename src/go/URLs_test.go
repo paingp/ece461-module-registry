9fe76a019f4565bd1d171e9aca3e6c138eb3acd6
paingp <pkhant@purdue.edu>
2023-03-04 16:41:14 -0500

    Refactor inherited code

--- src/go/URLs_test.go
+++ src/go/URLs_test.go
@@ -2,30 +2,30 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"testing"
 	"io/ioutil"
 	"log"
+	"os"
+	"testing"
+
+	"github.com/hugoday/ECE461ProjectCLI/src/go/ratom"
 )
-
-
 
 // * START OF RESPONSIVENESS * \\
 
 // Function to get responsiveness metric score
 func TestGetResponsiveness(t *testing.T) {
-	DebugLogger = log.New(new(NoLog), "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
-	InfoLogger = log.New(new(NoLog), "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ratom.DebugLogger = log.New(new(NoLog), "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ratom.InfoLogger = log.New(new(NoLog), "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 
 	testUrl := "github.com/hugoday/resume"
 	// cloneRepo(testUrl)
-	responsiveness := getResponsiveness(testUrl)
+	responsiveness := ratom.GetResponsiveness(testUrl)
 	fmt.Println(responsiveness)
 
 }
 
 func TestRemoveScores(t *testing.T) {
-	removeScores()
+	ratom.RemoveScores()
 }
 
 // * END OF RESPONSIVENESS * \\
@@ -35,7 +35,7 @@ func TestRemoveScores(t *testing.T) {
 // Function to get ramp-up time metric scor
 func TestGetRampUpTime(t *testing.T) {
 
-	getRampUpTime("github.com/hugoday/resume")
+	ratom.GetRampUpTime("github.com/hugoday/resume")
 }
 
 // * END OF RAMP-UP TIME * \\
@@ -51,187 +51,186 @@ func TestGetBusFactor(t *testing.T) {
 
 // * END OF BUS FACTOR * \\
 
-// * START OF CORRECTNESS * \\
+// * START OF Correctness * \\
 
-// Function to get correctness metric score
+// Function to get Correctness metric score
 func TestGetCorrectness(t *testing.T) {
 	rescueStdout := os.Stdout
-    read, w, _ := os.Pipe()
-    os.Stdout = w
+	read, w, _ := os.Pipe()
+	os.Stdout = w
 
 	// var r repo
 	// r.URL = "testUrl"
 	// r.netScore = 7.4
-	getCorrectness("not/a/url")
+	ratom.GetCorrectness("not/a/url")
 
-    w.Close()
-    out, _ := ioutil.ReadAll(read)
-    os.Stdout = rescueStdout
+	w.Close()
+	out, _ := ioutil.ReadAll(read)
+	os.Stdout = rescueStdout
 	// t.Errorf(string(out))
 	// t.Errorf("hi")
 	// t.Fail()
-    if string(out) != "Did not find issues file from api, invalid url: not/a/url\n" {
-        t.Errorf("Expected %s, got %s", "Did not find issues file from api, invalid url: not/a/url", out)
-    }
+	if string(out) != "Did not find issues file from api, invalid url: not/a/url\n" {
+		t.Errorf("Expected %s, got %s", "Did not find issues file from api, invalid url: not/a/url", out)
+	}
 }
 
 func TestRunRestApi(t *testing.T) {
 	rescueStdout := os.Stdout
-    read, w, _ := os.Pipe()
-    os.Stdout = w
+	read, w, _ := os.Pipe()
+	os.Stdout = w
 
 	// var r repo
 	// r.URL = "testUrl"
 	// r.netScore = 7.4
-	exitStatus := runRestApi("not/a/url")
+	exitStatus := ratom.RunRestApi("not/a/url")
 
-    w.Close()
-    out, _ := ioutil.ReadAll(read)
-    os.Stdout = rescueStdout
+	w.Close()
+	out, _ := ioutil.ReadAll(read)
+	os.Stdout = rescueStdout
 	// t.Errorf(string(out))
 	// t.Errorf("hi")
 	// t.Fail()
-    if exitStatus != 1 {
-        t.Errorf("Expected %s, got %s", "exit status 1", out)
-    }
+	if exitStatus != 1 {
+		t.Errorf("Expected %s, got %s", "exit status 1", out)
+	}
 }
 
 func TestTeardownRestApi(t *testing.T) {
 	rescueStdout := os.Stdout
-    read, w, _ := os.Pipe()
-    os.Stdout = w
+	read, w, _ := os.Pipe()
+	os.Stdout = w
 
+	ratom.TeardownRestApi()
 
-	teardownRestApi()
-
-    w.Close()
-    out, _ := ioutil.ReadAll(read)
-    os.Stdout = rescueStdout
+	w.Close()
+	out, _ := ioutil.ReadAll(read)
+	os.Stdout = rescueStdout
 	// t.Errorf(string(out))
 	// t.Errorf("hi")
 	// t.Fail()
-    if string(out) != "" {
-        t.Errorf("Expected %s, got %s", "nothing", out)
-    }
+	if string(out) != "" {
+		t.Errorf("Expected %s, got %s", "nothing", out)
+	}
 }
 
 func TestCalc_score(t *testing.T) {
 
-	out := calc_score("2", "2")
+	out := ratom.Calc_score("2", "2")
 
 	if out != 0.5 {
-        t.Errorf("Expected %s, got %f", "0.5", out)
-    }
+		t.Errorf("Expected %s, got %f", "0.5", out)
+	}
 }
 
-// * END OF CORRECTNESS * \\
+// * END OF Correctness * \\
 
 // * START OF LICENSE COMPATABILITY * \\
 
 // Function to get license compatibility metric score
 func TestGetLicenseCompatibility(t *testing.T) {
 	rescueStdout := os.Stdout
-    read, w, _ := os.Pipe()
-    os.Stdout = w
+	read, w, _ := os.Pipe()
+	os.Stdout = w
 
 	// var r repo
 	// r.URL = "testUrl"
 	// r.netScore = 7.4
-	getLicenseCompatibility("testURL")
+	ratom.GetLicenseCompatibility("testURL")
 
-    w.Close()
-    out, _ := ioutil.ReadAll(read)
-    os.Stdout = rescueStdout
+	w.Close()
+	out, _ := ioutil.ReadAll(read)
+	os.Stdout = rescueStdout
 	// t.Errorf(string(out))
 	// t.Errorf("hi")
 	// t.Fail()
-    if string(out) != "" {
-        t.Errorf("Expected %s, got %s", "nothing", out)
-    }
+	if string(out) != "" {
+		t.Errorf("Expected %s, got %s", "nothing", out)
+	}
 }
 
 func TestGetLicenseCompatibility2(t *testing.T) {
 	rescueStdout := os.Stdout
-    read, w, _ := os.Pipe()
-    os.Stdout = w
+	read, w, _ := os.Pipe()
+	os.Stdout = w
 
 	// var r repo
 	// r.URL = "testUrl"
 	// r.netScore = 7.4
-	getLicenseCompatibility("https://github.com/hugoday/resume")
+	ratom.GetLicenseCompatibility("https://github.com/hugoday/resume")
 
-    w.Close()
-    out, _ := ioutil.ReadAll(read)
-    os.Stdout = rescueStdout
+	w.Close()
+	out, _ := ioutil.ReadAll(read)
+	os.Stdout = rescueStdout
 	// t.Errorf(string(out))
 	// t.Errorf("hi")
 	// t.Fail()
-    if string(out) != "" {
-        t.Errorf("Expected %s, got %s", "nothing", out)
-    }
+	if string(out) != "" {
+		t.Errorf("Expected %s, got %s", "nothing", out)
+	}
 }
 
 func TestSearchForLicenses(t *testing.T) {
 	rescueStdout := os.Stdout
-    read, w, _ := os.Pipe()
-    os.Stdout = w
+	read, w, _ := os.Pipe()
+	os.Stdout = w
 
 	// var r repo
 	// r.URL = "testUrl"
 	// r.netScore = 7.4
-	searchForLicenses("./src")
+	ratom.SearchForLicenses("./src")
 
-    w.Close()
-    out, _ := ioutil.ReadAll(read)
-    os.Stdout = rescueStdout
+	w.Close()
+	out, _ := ioutil.ReadAll(read)
+	os.Stdout = rescueStdout
 	// t.Errorf(string(out))
 	// t.Errorf("hi")
 	// t.Fail()
-    if string(out) != "" {
-        t.Errorf("Expected %s, got %s", "nothing", out)
-    }
+	if string(out) != "" {
+		t.Errorf("Expected %s, got %s", "nothing", out)
+	}
 }
 
 func TestCheckFileForLicense1(t *testing.T) {
 	rescueStdout := os.Stdout
-    read, w, _ := os.Pipe()
-    os.Stdout = w
+	read, w, _ := os.Pipe()
+	os.Stdout = w
 
 	// var r repo
 	// r.URL = "testUrl"
 	// r.netScore = 7.4
-	checkFileForLicense("./src/does/not/exist")
+	ratom.CheckFileForLicense("./src/does/not/exist")
 
-    w.Close()
-    out, _ := ioutil.ReadAll(read)
-    os.Stdout = rescueStdout
+	w.Close()
+	out, _ := ioutil.ReadAll(read)
+	os.Stdout = rescueStdout
 	// t.Errorf(string(out))
 	// t.Errorf("hi")
 	// t.Fail()
-    if string(out) != "" {
-        t.Errorf("Expected %s, got %s", "nothing", out)
-    }
+	if string(out) != "" {
+		t.Errorf("Expected %s, got %s", "nothing", out)
+	}
 }
 
 func TestCheckFileForLicense2(t *testing.T) {
 	rescueStdout := os.Stdout
-    read, w, _ := os.Pipe()
-    os.Stdout = w
+	read, w, _ := os.Pipe()
+	os.Stdout = w
 
 	// var r repo
 	// r.URL = "testUrl"
 	// r.netScore = 7.4
-	checkFileForLicense("main.go")
+	ratom.CheckFileForLicense("main.go")
 
-    w.Close()
-    out, _ := ioutil.ReadAll(read)
-    os.Stdout = rescueStdout
+	w.Close()
+	out, _ := ioutil.ReadAll(read)
+	os.Stdout = rescueStdout
 	// t.Errorf(string(out))
 	// t.Errorf("hi")
 	// t.Fail()
-    if string(out) != "" {
-        t.Errorf("Expected %s, got %s", "nothing", out)
-    }
+	if string(out) != "" {
+		t.Errorf("Expected %s, got %s", "nothing", out)
+	}
 }
 
 // * END OF LICENSE COMPATABILITY * \\
@@ -239,11 +238,11 @@ func TestCheckFileForLicense2(t *testing.T) {
 // * START OF REPO CLONING/REMOVING  * \\
 
 func TestCloneRepo(t *testing.T) {
-	cloneRepo("not/a/repo")
+	ratom.CloneRepo("not/a/repo")
 }
 
 func TestClearRepoFolder(t *testing.T) {
-	clearRepoFolder()
+	ratom.ClearRepoFolder()
 }
 
 // * END OF REPO CLONING/REMOVING  * \\
@@ -251,31 +250,31 @@ func TestClearRepoFolder(t *testing.T) {
 // * START OF STDOUT * \\
 
 func TestPrintRepo(t *testing.T) {
-	r := repo{URL: "testRepo"}
-	r.busFactor = -1
-	r.correctness = -1
-	r.licenseCompatibility = -1
-	r.rampUpTime = -1
-	r.netScore = -1
-	printRepo(&r)
+	r := ratom.Repo{URL: "testRepo"}
+	r.BusFactor = -1
+	r.Correctness = -1
+	r.LicenseCompatibility = -1
+	r.RampUpTime = -1
+	r.NetScore = -1
+	ratom.PrintRepo(&r)
 }
 
 func TestRepoOUT(t *testing.T) {
 	rescueStdout := os.Stdout
-    read, w, _ := os.Pipe()
-    os.Stdout = w
+	read, w, _ := os.Pipe()
+	os.Stdout = w
 
-	var r repo
+	var r ratom.Repo
 	r.URL = "testUrl"
-	r.netScore = 7.4
-	repoOUT(&r)
+	r.NetScore = 7.4
+	ratom.RepoOUT(&r)
 
-    w.Close()
-    out, _ := ioutil.ReadAll(read)
-    os.Stdout = rescueStdout
-    if string(out) != "{\"URL\":\"testUrl\", \"NET_SCORE\":7.40, \"RAMP_UP_SCORE\":0.00, \"CORRECTNESS_SCORE\":0.00, \"BUS_FACTOR_SCORE\":0.00, \"RESPONSIVE_MAINTAINER_SCORE\":0.00, \"LICENSE_SCORE\":0.00} \n" {
-        t.Errorf("Expected %s, got %s", "{\"URL\":\"testUrl\", \"NET_SCORE\":7.40, \"RAMP_UP_SCORE\":0.00, \"CORRECTNESS_SCORE\":0.00, \"BUS_FACTOR_SCORE\":0.00, \"RESPONSIVE_MAINTAINER_SCORE\":0.00, \"LICENSE_SCORE\":0.00}", out)
-    }
+	w.Close()
+	out, _ := ioutil.ReadAll(read)
+	os.Stdout = rescueStdout
+	if string(out) != "{\"URL\":\"testUrl\", \"NET_SCORE\":7.40, \"RAMP_UP_SCORE\":0.00, \"Correctness_SCORE\":0.00, \"BUS_FACTOR_SCORE\":0.00, \"RESPONSIVE_MAINTAINER_SCORE\":0.00, \"LICENSE_SCORE\":0.00} \n" {
+		t.Errorf("Expected %s, got %s", "{\"URL\":\"testUrl\", \"NET_SCORE\":7.40, \"RAMP_UP_SCORE\":0.00, \"Correctness_SCORE\":0.00, \"BUS_FACTOR_SCORE\":0.00, \"RESPONSIVE_MAINTAINER_SCORE\":0.00, \"LICENSE_SCORE\":0.00}", out)
+	}
 
 }
 
@@ -284,5 +283,5 @@ func TestRepoOUT(t *testing.T) {
 // * START OF SORTING * \\
 
 func TestAddRepo(t *testing.T) {
-	
+
 }
--- src/go/main.go
+++ src/go/main.go
@@ -6,13 +6,10 @@ import (
 	"log"
 	// "math"
 	// "utils"
-	"fmt"
-	"os"
-)
 
-var (
-	DebugLogger *log.Logger
-	InfoLogger  *log.Logger
+	"os"
+
+	"github.com/hugoday/ECE461ProjectCLI/src/go/ratom"
 )
 
 type NoLog int
@@ -22,8 +19,6 @@ func (NoLog) Write([]byte) (int, error) {
 }
 
 func main() {
-
-	fmt.Println("Aditya Srikanth")
 
 	doLogging := true
 	logFileName := os.Getenv("LOG_FILE")
@@ -35,38 +30,38 @@ func main() {
 
 	if doLogging {
 		if logLevel == "2" {
-			DebugLogger = log.New(logFile, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
+			ratom.DebugLogger = log.New(logFile, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
 		} else {
-			DebugLogger = log.New(new(NoLog), "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
+			ratom.DebugLogger = log.New(new(NoLog), "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
 		}
-		InfoLogger = log.New(logFile, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+		ratom.InfoLogger = log.New(logFile, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 	} else {
-		DebugLogger = log.New(new(NoLog), "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
-		InfoLogger = log.New(new(NoLog), "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+		ratom.DebugLogger = log.New(new(NoLog), "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
+		ratom.InfoLogger = log.New(new(NoLog), "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 	}
 
 	// Makes sure repository folder is clear
-	clearRepoFolder()
+	ratom.ClearRepoFolder()
 
 	// Opens URL file and creates a scanner
 	file, _ := os.Open(os.Args[1])
 	scanner := bufio.NewScanner(file)
 
 	// Create head and temporary repo nodes
-	var head *repo
-	var hold *repo
-	head = &repo{URL: "HEAD"}
+	var head *ratom.Repo
+	var hold *ratom.Repo
+	head = &ratom.Repo{URL: "HEAD"}
 
-	InfoLogger.Println("Beginning URL file read")
+	ratom.InfoLogger.Println("Beginning URL file read")
 	// for each url in the file
 	for scanner.Scan() {
 		//Create new repositories with current URL scanned
-		hold = newRepo(scanner.Text())
-		InfoLogger.Println("New repo created successfully")
+		hold = ratom.NewRepo(scanner.Text())
+		ratom.InfoLogger.Println("New repo created successfully")
 		// Adds repository to Linked List in sorted order by net score
-		head = addRepo(head, head.next, hold)
+		head = ratom.AddRepo(head, head.Next, hold)
 	}
 
 	// Prints each repository in NDJSON format to stdout (sorted highest to low based off net score)
-	printRepo(head.next)
+	ratom.PrintRepo(head.Next)
 }
