e715643f240b177280de8cbf5fa686bf71c1495f
AdityaSri11 <srikant2@purdue.edu>
2023-04-08 15:46:29 -0400

    Regex searching

---
+++ src/gcp_calc/regex.py
@@ -0,0 +1,45 @@
+import os
+import re
+from google.cloud import storage
+
+os.environ["GCLOUD_PROJECT"] = "ece461-module-registry"
+bucket_name = "tmr-bucket"
+
+def find_regex_matches(regex_string):
+
+    storage_client = storage.Client()
+    blobs = storage_client.list_blobs(bucket_name)
+
+    regex_pattern = re.compile(regex_string)
+
+    results = []
+
+    for blob in blobs:
+
+        blobWritten = 0
+        result_append = []
+        
+        if regex_pattern.match(blob.name) != None:
+            moduleName = blob.name[:-4]
+            result_append.append(moduleName)
+
+            moduleVersion = (blob.metadata)['version']
+            result_append.append(moduleVersion)
+
+            blobWritten = 1
+        
+        if regex_pattern.match((blob.metadata)['README']) != None and blobWritten == 0:
+            moduleName = blob.name[:-4]
+            result_append.append(moduleName)
+
+            moduleVersion = (blob.metadata)['version']
+            result_append.append(moduleVersion)
+
+            blobWritten = 1
+
+        if blobWritten == 1:
+            results.append(result_append)
+
+    return results
+
+print(find_regex_matches("lodash"))
