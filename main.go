291e963d5e24fc18f011ac238d50361d5ddbe146
paingp <pkhant@purdue.edu>
2023-04-22 16:30:01 -0400

    main.go for testing

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ import (
 	"tomr/src/utils"
 )
 
-// "fmt"
-
 func main() {
 	//url := "https://www.npmjs.com/package/du"
 
@@ -34,36 +32,37 @@ func main() {
 		}
 	*/
 	/*
-		router := chi.NewRouter()
+				router := chi.NewRouter()
 
-		router.Put("/authenticate", handlers.CreateAuthToken)
+				router.Put("/authenticate", handlers.CreateAuthToken)
 
-		router.Get("/", func(writer http.ResponseWriter, request *http.Request) {
-			_, err := writer.Write([]byte("ece461g17-module-registry"))
-			if err != nil {
-				log.Println(err)
-			}
-		})
+				router.Get("/", func(writer http.ResponseWriter, request *http.Request) {
+					_, err := writer.Write([]byte("ece461g17-module-registry"))
+					if err != nil {
+						log.Println(err)
+					}
+				})
 
-		router.Route("/package", func(r chi.Router) {
-			// r.Post("/", handlers.CreatePackage)
-			r.Get("/{id}", handlers.RetrievePackage)
-			r.Put("/{id}", handlers.UpdatePackage)
-			r.Delete("/{id}", handlers.DeletePackage)
-			r.Get("/{id}/rate", handlers.RatePackage)
-		})
+				router.Route("/package", func(r chi.Router) {
+					// r.Post("/", handlers.CreatePackage)
+					r.Get("/{id}", handlers.RetrievePackage)
+					r.Put("/{id}", handlers.UpdatePackage)
+					r.Delete("/{id}", handlers.DeletePackage)
+					r.Get("/{id}/rate", handlers.RatePackage)
+				})
 
-		router.Get("/package/byName/{name}", handlers.GetPackageByName)
-		router.Delete("/package/byName/{name}", handlers.DeletePackageByName)
+				router.Get("/package/byName/{name}", handlers.GetPackageByName)
+				router.Delete("/package/byName/{name}", handlers.DeletePackageByName)
 
-		router.Post("/package/byRegEx", handlers.GetPackageByRegEx)
+				router.Post("/package/byRegEx", handlers.GetPackageByRegEx)
 
-		router.Post("/packages", handlers.ListPackages)
+				router.Post("/packages", handlers.ListPackages)
 
-		router.Delete("/reset", handlers.ResetRegistry)
+				router.Delete("/reset", handlers.ResetRegistry)
 
-    err := http.ListenAndServe(":3000", router)
-    if err != nil {
-        log.Println(err)
-    }
+		    err := http.ListenAndServe(":3000", router)
+		    if err != nil {
+		        log.Println(err)
+		    }
+	*/
 }
