311514249f6a25be4ca426f5144b2636d5edea4b
AdityaSri11 <srikant2@purdue.edu>
2023-04-23 22:16:17 -0400

    fixed and tested authentication token

--- main.go
+++ main.go
@@ -42,8 +42,8 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		// port = "8080"
-		port = "3000"
+		port = "8080"
+		// port = "3000"
 	}
 	fmt.Printf("Server started on PORT %s\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, router))
